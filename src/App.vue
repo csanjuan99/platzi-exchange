29e2a3a40d13f1d7b544644ee589b7883ffacc73
Carlos Sanjuan <carlosdsanjuan@outlook.com>
2022-01-11 08:22:10 -0500

    Platzi Exchange V2.O Basic Course Projet

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <px-header />
+
+    <router-view class="w-full" />
+  </main>
+</template>
+
+<script>
+import PxHeader from '@/components/PxHeader'
+
+export default {
+  name: 'app',
+  components: { PxHeader }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
